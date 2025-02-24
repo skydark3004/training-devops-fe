2df7a7dc9b451949f2efc5d11e40a233448aaf62
huynh_systemi <nguyen.huu.huy@systemi.co.jp>
2025-02-24 15:49:11 +0700

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,4 @@
 module.exports = {
-  apps: [
-    {
-      name: 'front-end-development',
-      script: '.next/standalone/server.js',
-      instances: '2',
-      exec_mode: 'cluster',
-      watch: false,
-      ignore_watch: ['node_modules', 'logs'],
-      output: './logs/out.json',
-      error: './logs/error.json',
-      log_type: 'json',
-      merge_logs: true,
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      log_file: './logs/combined.json',
-      max_memory_restart: '500M',
-      pid_file: './logs/app.pid',
-      autorestart: true,
-      restart_delay: 5000,
-      min_uptime: '60s',
-      max_restarts: 5,
-      shutdown_with_message: true,
-      time: true,
-    },
-  ],
   deploy: {
     development: {
       user: 'thangl-vietis',
